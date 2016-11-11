03bd6153289c6a435cfb56e0b028ad80a55264a0
Christopher Getschmann <christopher@volzo.de>
2016-11-11 20:47:57 +0100

    Data Collector for accelerometer samples added

--- Tapper/app/src/main/java/de/volzo/tapper/MainActivity.java
+++ Tapper/app/src/main/java/de/volzo/tapper/MainActivity.java
@@ -3,11 +3,17 @@ package de.volzo.tapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.volzo.tapper.GestureDetector.DataCollector;
+
 public class MainActivity extends AppCompatActivity {
+
+    DataCollector dataCollector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataCollector = new DataCollector(this);
     }
 }
