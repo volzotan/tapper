69c308e31149354f89bd94ce0493ecc9afa20607
Tassilo Karge <tassilo.karge@uni-konstanz.de>
2016-12-14 15:41:59 +0100

    added interface to GestureType for descriptions and display names

--- Tapper/app/src/main/java/de/volzo/tapper/GestureDetector/GestureType.java
+++ Tapper/app/src/main/java/de/volzo/tapper/GestureDetector/GestureType.java
@@ -1,13 +1,41 @@
 package de.volzo.tapper.GestureDetector;
 
+import java.util.HashMap;
+
 /**
  * Created by volzotan on 11.11.16.
  */
 public enum GestureType {
-    NOTHING,
+    NOTHING, //only used internally
     DOUBLETAP,
     SIDETAP,
-    TAP,
+    TAP, //only used internally
     PICKUPDROP,
-    SHAKE
+    SHAKE;
+
+    static final HashMap<GestureType, String> displayNames = new HashMap<GestureType, String>() {{
+        put(DOUBLETAP, "Double Tap");
+        put(SIDETAP, "Side Tap");
+        put(PICKUPDROP, "Pick up, Drop");
+        put(SHAKE, "Shake");
+    }};
+
+    static final HashMap<GestureType, String> descriptions = new HashMap<GestureType, String>() {{
+        put(DOUBLETAP, "Knocking with the knuckles twice");
+        put(SIDETAP, "A quick slap on the side");
+        put(PICKUPDROP, "Picking up and dropping, like you want to get rid of the ringing of an old analogue phone");
+        put(SHAKE, "Shake for three or four times, like waking someone up");
+    }};
+
+    public GestureType[] getAllPublicGestureTypes() {
+        return new GestureType[]{DOUBLETAP, SIDETAP, PICKUPDROP, SHAKE};
+    }
+
+    public String getDescription(GestureType type) {
+        return descriptions.get(type);
+    }
+
+    public String getDisplayName(GestureType type) {
+        return descriptions.get(type);
+    }
 }
